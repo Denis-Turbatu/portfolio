671d7dde7b626bb2ada8358e4ca02e9ae65859c8
Denis-Turbatu <turbatudenis81@gmail.com>
2024-08-20 15:25:50 +0200

    fix: require on Appheader.vue for the logo

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -81,7 +81,7 @@
         { 'rounded-lg mx-4 my-2 [&>ul>li]:flex justify-center items-center': isSticky }
       ]">
             <a href="#" class="flex items-center space-x-3 rtl:space-x-reverse">
-                <img :src="`../src/assets/${isDark ? 'logo-light' : 'logo'}.png`"
+                <img :src="getImageUrl(isDark ? 'logo-light.png' : 'logo.png')"
                     class="h-8 transition-all duration-300" alt="logo" />
                 <span v-if="!isSticky"
                     class="self-center text-2xl font-semibold whitespace-nowrap dark:text-platinum transition-all duration-300">
